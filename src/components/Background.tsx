3a0acc2efcd9d2b130b3000eb648c0534de045c3
Sophie Stammers <sohyphae@gmail.com>
2025-10-09 23:32:42 +0100

    Add Background and Header

---
+++ src/components/Background.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import'../App.css';
+
+const Background: React.FC = () => {
+  return <div className="background"></div>;
+};
+
+export default Background;
---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <section id="header" className="header">
+      <div className="logo">
+        <h1>
+            <span>S</span>
+            <span>O</span>
+            <span>P</span>
+            <span>H</span>
+            <span>I</span>
+            <span>E</span>
+        </h1>    
+                <h1>
+            <span>H</span>
+            <span>Y</span>
+            <span>P</span>
+            <span>H</span>
+            <span>A</span>
+            <span>E</span>
+        </h1>  
+      </div>
+    </section>
+  );
+};
+
+export default Header;
