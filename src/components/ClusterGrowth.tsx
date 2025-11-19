ecc56628c087624e4800635ac5dfda0a5732b9c7
Sophie Stammers <sohyphae@gmail.com>
2025-11-19 17:55:25 +0000

    better dimension og img

--- src/components/ClusterGrowth.tsx
+++ src/components/ClusterGrowth.tsx
@@ -54,8 +54,9 @@ const ClusterGrowth: React.FC = () => {
 
     // Function to draw a single cell
     function drawCell(x: number, y: number) {
-      ctx.fillStyle = "#0f013305";
+      // ctx.fillStyle = "#0f013305";
       // ctx.fillStyle = "#1c016205";
+      ctx.fillStyle = "#00176c05";
       // ctx.fillStyle = "#402e9003";
       let xPos = Math.round(x * totalOffset - cellSize / 2);
       let yPos = Math.round(y * totalOffset - cellSize / 2)
--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -6,7 +6,7 @@ const Live: React.FC = () => {
       <div className="live-container">
         <h2 className="section-header text-mid-dark">Live Dates</h2>
         <h2 className="dates-header text-mid-dark">Upcoming</h2>
-        <p className="body-text text-dark">18th December 2025: The Canteen (The Mixed Dips)</p>
+        <p className="body-text text-dark">18th December 2025: The Canteen, Bristol (The Mixed Dips)</p>
         <h2 className="dates-header text-mid-dark">Past</h2>
         <p className="body-text text-dark">22nd August 2025: Shambala Festival (The Mixed Dips)</p>
         <p className="body-text text-dark">11th April 2025: Zoophoria, The Exchange Bristol (The Mixed Dips)</p>
