0b30a30a75503a6830ec3aa4ba4a977c9ffb53c2
Sophie Stammers <sohyphae@gmail.com>
2026-04-12 10:41:00 +0100

    update blurb and live dates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,10 @@ const About: React.FC = () => {
       <div className="about-container">
         <div className="about-text">
           <h2 className="section-header text-mid-dark margin-top-10vh">About</h2>
-          <p className="body-text text-dark">Sophie Hyphae weaves glimmering synth and piano improvisations with ethereal vocal loops and ambient soundscapes, to create dreamlike, nature-entangled songs.</p> 
+          <p className="body-text text-dark">Sophie Hyphae weaves glimmering piano improvisations with ethereal vocal loops, ambient soundscapes, and curious earthy lyrics, to create dreamlike, nature-entangled, organic electronica.</p>
+          <p className="body-text text-dark">Shimmering synths and soaring vocals weave an impressionistic story of how connection with nature, embracing sensitivity, and finding kindred good souls will win over disconnection and accelerationism.</p>  
           <p className="body-text text-dark">
-            It's the latest project from Sophie Stammers, multi-instrumentalist and songwriter based in Bristol. A classically trained pianist, for the last decade Sophie had mostly ignored the piano, in favour of instruments she could (just about!) throw on her bike and cycle to the next session. But after taking in a friend's piano that needed a home, keys returned to Sophie's musical life in a big way, and brought with them a rekindled intrest in synthesizers and live looping. Sophie layers up the majority of her sounds live on the Nord Stage 4 and the Boss RC300 loopstation.   
+          Sophie sculpts and layers up the majority of her sounds live when she performs. She does this by programming multiple synthesised sounds to follow and respond to the piano melody, generating rich, evolving textures on the fly. With her feet, she controls a loopstation, which allows her to further build, switch and activate different aspects of the music. These responsive layers bloom outward in real time, and with the tactile intimacy of the piano, together with sampled sounds from nature, she invites listeners into a world where electronics and ecology intertwine.  
           </p>
           <p className="body-text text-dark">
             Sophie is also synth chef and vocalist for <a href="https://www.themixeddips.com/" target="_blank" rel="noopener noreferrer" >The Mixed Dips</a>, a live band that reimagines rave classics, fronted by drag queen DJ <a href="https://www.instagram.com/gaba_ghanoush/" target="_blank" rel="noopener noreferrer" >Gaba Ghanoush</a>.
--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -6,8 +6,12 @@ const Live: React.FC = () => {
       <div className="live-container">
         <h2 className="section-header text-mid-dark">Live Dates</h2>
         <h2 className="dates-header text-mid-dark">Upcoming</h2>
-        <p className="body-text text-dark">18th December 2025: The Canteen, Bristol (The Mixed Dips)</p>
+        <p className="body-text text-dark">18th April 2026: Dubber's End Spring Fiesta</p>
+        <p className="body-text text-dark">1st-4th May 2026: Insieme Festival</p>
+        <p className="body-text text-dark">3rd-6th July 2026: Not A Cult Festival</p>
+        <p className="body-text text-dark">25th July 2026: Windsor and Eton Pride</p>
         <h2 className="dates-header text-mid-dark">Past</h2>
+        <p className="body-text text-dark">18th December 2025: The Canteen, Bristol (The Mixed Dips)</p>
         <p className="body-text text-dark">22nd August 2025: Shambala Festival (The Mixed Dips)</p>
         <p className="body-text text-dark">11th April 2025: Zoophoria, The Exchange Bristol (The Mixed Dips)</p>
         <p className="body-text text-dark">23rd June 2024: Chelsea Physic Garden Summer Lates</p>
